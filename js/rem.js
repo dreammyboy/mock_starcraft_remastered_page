0e73eba228aa55515f281420dd0bdda5d6b0ed88
Dong Zhe(视频) <zhedong@sohu-inc.com>
2018-06-26 19:28:56 +0800

    init

---
+++ js/rem.js
@@ -0,0 +1,8 @@
+/**
+ * Created by zhedong on 2017/6/13.
+ */
+!function() {
+    var doc = document.documentElement;
+    var width = doc.clientWidth;
+    doc.style.fontSize = doc.clientWidth/10 + 'px';
+}();
